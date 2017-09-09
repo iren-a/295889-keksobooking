78bd5c71aa714f6efd17ab1297ba0d450e5b3a58
Irina Arinushkina <ira.arinushkina@gmail.com>
2017-09-09 21:55:32 +0300

    #20 Личный проект: надо подкачаться

--- js/pin.js
+++ js/pin.js
@@ -5,66 +5,44 @@
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
 
-  function renderPinMap(offersArr) {
-    var tokyoPinMap = document.querySelector('.tokyo__pin-map');
-    var fragment = document.createDocumentFragment();
+  function addPinHandlers(pin, offer) {
 
-    for (var i = 0; i < offersArr.length; i++) {
-      var div = document.createElement('div');
-      div.classList.add('pin');
-      div.style.left = offersArr[i].location.x + 'px';
-      div.style.top = offersArr[i].location.y + 'px';
-      var img = document.createElement('img');
-      img.src = offersArr[i].author.avatar;
-      img.classList.add('rounded');
-      img.width = 40;
-      img.height = 40;
-      img.setAttribute('tabindex', '1');
-      div.appendChild(img);
+    pin.addEventListener('click', pinClickHandler);
+    pin.addEventListener('keydown', pinEnterPressHandler);
 
-      fragment.appendChild(div);
+    function openDialog() {
+      var activePin = document.querySelector('.pin--active');
+      if (activePin) {
+        activePin.classList.remove('pin--active');
+      }
+      pin.classList.add('pin--active');
+
+      dialog.classList.remove('hidden');
+      window.showCard(offer);
     }
-    tokyoPinMap.appendChild(fragment);
+
+    function pinClickHandler() {
+      openDialog();
+      document.addEventListener('keydown', dialogEscPressHandler);
+    }
+
+    function pinEnterPressHandler(evt) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        openDialog();
+        document.addEventListener('keydown', dialogEscPressHandler);
+      }
+    }
   }
 
-  renderPinMap(window.offers);
-
-  var pins = document.querySelectorAll('.pin:not(.pin__main)');
   var dialogClose = document.querySelector('.dialog__close');
   var dialog = document.querySelector('.dialog');
 
-  function openDialog(idx) {
-    for (var i = 0; i < pins.length; i++) {
-      pins[i].classList.remove('pin--active');
-    }
-    pins[idx].classList.add('pin--active');
-
-    dialog.classList.remove('hidden');
-    window.showCard(pins[idx]);
-  }
-
   function closeDialog() {
     dialog.classList.add('hidden');
-    for (var j = 0; j < pins.length; j++) {
-      pins[j].classList.remove('pin--active');
+    var activePin = document.querySelector('.pin--active');
+    if (activePin) {
+      activePin.classList.remove('pin--active');
     }
-  }
-
-  function pinClickHandler(i) {
-    openDialog(i);
-    document.addEventListener('keydown', dialogEscPressHandler);
-  }
-
-  function pinEnterPressHandler(i, evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
-      openDialog(i);
-      document.addEventListener('keydown', dialogEscPressHandler);
-    }
-  }
-
-  for (var i = 0; i < pins.length; i++) {
-    pins[i].addEventListener('click', pinClickHandler.bind(null, i));
-    pins[i].addEventListener('keydown', pinEnterPressHandler.bind(null, i));
   }
 
   dialogClose.addEventListener('click', function () {
@@ -78,5 +56,35 @@
       document.removeEventListener('keydown', dialogEscPressHandler);
     }
   }
+
+  function loadHandler(offersArr) {
+    var tokyoPinMap = document.querySelector('.tokyo__pin-map');
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < offersArr.length; i++) {
+      var pin = document.createElement('div');
+      pin.classList.add('pin');
+      pin.style.left = offersArr[i].location.x + 'px';
+      pin.style.top = offersArr[i].location.y + 'px';
+      var img = document.createElement('img');
+      img.src = offersArr[i].author.avatar;
+      img.classList.add('rounded');
+      img.width = 40;
+      img.height = 40;
+      img.setAttribute('tabindex', '1');
+      pin.appendChild(img);
+
+      addPinHandlers(pin, offersArr[i]);
+
+      fragment.appendChild(pin);
+    }
+    tokyoPinMap.appendChild(fragment);
+  }
+
+  function errorHandler(errorMessage) {
+    window.utils.showErrorMessage(errorMessage);
+  }
+
+  window.backend.load(loadHandler, errorHandler);
 
 })();
